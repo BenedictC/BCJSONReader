e50bac714683f7bd810456a48ca09628ee9b73ed
Benedict Cohen <ben@benedictcohen.co.uk>
2014-10-26 09:05:14 +0000

    - Added __attribute__((nonnull(...)))

---
+++ BCMJSONAccess/BCMJSONAccess.h
@@ -0,0 +1,13 @@
+//
+//  BCMJSONAccess.h
+//  BCMJSONAccess
+//
+//  Created by Benedict Cohen on 26/10/2014.
+//  Copyright (c) 2014 Benedict Cohen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BCMJSONAccess : NSObject
+
+@end
---
+++ BCMJSONAccess/BCMJSONAccess.m
@@ -0,0 +1,13 @@
+//
+//  BCMJSONAccess.m
+//  BCMJSONAccess
+//
+//  Created by Benedict Cohen on 26/10/2014.
+//  Copyright (c) 2014 Benedict Cohen. All rights reserved.
+//
+
+#import "BCMJSONAccess.h"
+
+@implementation BCMJSONAccess
+
+@end
