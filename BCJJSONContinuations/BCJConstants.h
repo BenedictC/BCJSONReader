1f973e8fa18b2587cf6fdcb8e76259d1d1d33eae
Benedict Cohen <ben@benedictcohen.co.uk>
2014-11-12 13:52:20 +0000

    - Added documentation

--- BCJJSONContinuations/BCJConstants.h
+++ BCJJSONContinuations/BCJConstants.h
@@ -11,14 +11,29 @@
 
 
 #pragma mark - Errors
+/**
+ The BCJJSONContinuation error domain.
+ */
 extern NSString * const BCJErrorDomain;
 
+/**
+ userInfo key for the BCJJSONSource instance involved in the error.
+ */
 extern NSString * const BCJJSONSourceErrorKey;
 
+/**
+ userInfo key for the enumMapping NSDictionary involved in a BCJUnknownEnumMappingKeyError error.
+ */
 extern NSString * const BCJEnumMappingErrorKey;
 
+/**
+ userInfo key for the underlying NSError.
+ */
 extern NSString * const BCJUnderlyingErrorKey;
 
+/**
+ userInfo key for the NSNumber position.
+ */
 extern NSString * const BCJInvalidJSONPathFailurePositionErrorKey;
 
 
--- BCJJSONContinuations/BCJContainer.h
+++ BCJJSONContinuations/BCJContainer.h
@@ -11,14 +11,38 @@
 #import "BCJContainerProtocols.h"
 
 
+/**
+ BCJContainer implements the 2 BCJContainer protocols and acts as a wrapper for defering JSON deserialization. 
+ 
+ The contents of the container can be set only once. Once the content is set the container becomes sealed. Attempting to set the content on sealed container will result in an exception being raised.
 
+ */
 @interface BCJContainer : NSObject <BCJIndexedContainer, BCJKeyedContainer>
 
+/**
+ Initializes the container with the supplied content.
+
+ @param content The content. Raises an exception if content is nil.
+
+ @return A container
+ */
 -(instancetype)initWithContent:(id)content;
 
+/**
+ The containers content.
+ */
 @property(nonatomic, readonly) id content;
-@property(nonatomic, readonly) BOOL isSealed;
 
+/**
+ The sealed status of the container.
+ */
+@property(nonatomic, readonly, getter = isSealed) BOOL sealed;
+
+/**
+ Sets the content and seals the container. Raises an exception if the container is already sealed.
+
+ @param object the object to use as the content. Raises an exception if nil.
+ */
 -(void)setContentAndSeal:(id)object;
 
 @end
