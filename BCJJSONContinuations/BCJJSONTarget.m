e459a130239dd180be555acae1069cc7f0c0179c
Benedict Cohen <ben@benedictcohen.co.uk>
2014-11-16 11:24:24 +0000

    - Tidy

--- BCJJSONContinuations/BCJJSONTarget.m
+++ BCJJSONContinuations/BCJJSONTarget.m
@@ -10,8 +10,82 @@
 
 #import "BCJDefines.h"
 #import "BCJError.h"
+
+
 #ifdef DEBUG
 #import <objc/runtime.h>
+
+
+/**
+ //KVC will work regardless of type which means type mismatch bugs can occur. We add type checking for DEBUG builds
+ //to catch these bugs early.
+
+ @param object The object that will receive the value.
+ @param key    The KVC key for the value to be set.
+ @param value  The value to set.
+
+ */
+static inline void assertKVCTargetWillAcceptValue(id object, NSString *key, id value) {
+
+    //0. ensure we have a value to check
+    if (value == nil) return;
+
+    //1. Look for a *property* matching targetKey
+    objc_property_t property = class_getProperty([object class], key.UTF8String);
+    if (property != NULL) {
+        NSString *className = ({
+            NSString *propertyString = [NSString stringWithUTF8String:property_getAttributes(property)];
+
+            NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^T@\"([^\"]*)\"" options:0 error:NULL];
+            NSTextCheckingResult *match = [regex firstMatchInString:propertyString options:0 range:NSMakeRange(0, [propertyString length])];
+            NSRange range = [match rangeAtIndex:1];
+            (match == nil) ? nil : [propertyString substringWithRange:range];
+        });
+
+        if (className != nil) {
+            Class class = NSClassFromString(className);
+            NSCAssert([value isKindOfClass:class], ({
+                [NSString stringWithFormat:@"Attempted to set an object of type <%@> to an ivar of type <%@> for key <%@> of object <%@>.", NSStringFromClass([value class]), NSStringFromClass(class), key, object];
+            }));
+            return;
+        }
+        //TODO: Check NSValue.objCType types
+    }
+
+    //2. Look for an *ivar* matching targetKey
+    unsigned int outCount;
+    Ivar *ivars = class_copyIvarList([object class], &outCount);
+    //Wrap the buffer so that it's automatically freed when we return.
+    NSData *ivarsData = [NSData dataWithBytesNoCopy:ivars length:sizeof(ivars) * outCount freeWhenDone:YES];
+    for (unsigned int i = 0; i < outCount; i++) {
+        Ivar ivar = ivars[i];
+        NSString *ivarName = [NSString stringWithFormat:@"%s", ivar_getName(ivar)];
+        BOOL isMatch = ([ivarName isEqualToString:key]) || [ivarName isEqualToString:[@"_" stringByAppendingString:key]];
+        if (!isMatch) continue;
+
+        NSString *encoding = [NSString stringWithFormat:@"%s", ivar_getTypeEncoding(ivar)];
+        BOOL isObject = [encoding hasPrefix:@"@"];
+        if (isObject) {
+            NSRange range = NSMakeRange(2, encoding.length-3);
+            NSString *className = [encoding substringWithRange:range];
+            Class class = NSClassFromString(className);
+            NSCAssert([value isKindOfClass:class], ({
+                [NSString stringWithFormat:@"Attempted to set an object of type <%@> to an ivar of type <%@> for key <%@> of object <%@>.", NSStringFromClass([value class]), NSStringFromClass(class), key, object];
+            }));
+            return;
+        }
+        //TODO: Check NSValue.objCType types
+    }
+    [ivarsData self]; //Force the buffer to be retained until the end of the function. We do this rather than free() so that if we return early we don't acciendently leak.
+
+}
+
+#else
+
+static inline void assertKVCTargetWillAcceptValue(id object, NSString *key, id value) {
+    //Do nothing in release builds.
+}
+
 #endif
 
 
@@ -43,89 +117,12 @@
 
 -(BOOL)setValue:(id)value error:(NSError **)outError;
 {
-    //Ensure we're sane
-    if (self.object == nil) {
-        if (outError != NULL) {
-#pragma message "TODO: cannot set value on nil object."
-            *outError = [NSError new];
-        }
-        return NO;
-    }
-
-    if (self.key == nil || self.key.length == 0) {
-        if (outError != NULL) {
-#pragma message "TODO: cannot set value using empty key."
-            *outError = [NSError new];
-        }
-        return NO;
-    }
-
-#ifdef DEBUG
-    //KVC will work regardless of type which means type mismatch bugs can occur. We add type checking for DEBUG builds
-    //to catch these bugs early.
-
-    ^{
-        //0. ensure we have a value to check
-        if (value == nil) return;
-
-        //1. Look for a *property* matching targetKey
-        objc_property_t property = class_getProperty([self.object class], self.key.UTF8String);
-        if (property != NULL) {
-            NSString *className = ({
-                NSString *propertyString = [NSString stringWithUTF8String:property_getAttributes(property)];
-
-                NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^T@\"([^\"]*)\"" options:0 error:NULL];
-                NSTextCheckingResult *match = [regex firstMatchInString:propertyString options:0 range:NSMakeRange(0, [propertyString length])];
-                NSRange range = [match rangeAtIndex:1];
-                (match == nil) ? nil : [propertyString substringWithRange:range];
-            });
-
-            if (className != nil) {
-                Class class = NSClassFromString(className);
-                NSCAssert([value isKindOfClass:class], ({
-                    [NSString stringWithFormat:@"Attempted to set an object of type <%@> to an ivar of type <%@> for key <%@> of object <%@>.", NSStringFromClass([value class]), NSStringFromClass(class), self.key, self.object];
-                }));
-                return;
-            }
-            //TODO: Check non-object types
-        }
-
-        //2. Look for an *ivar* matching targetKey
-        unsigned int outCount;
-        Ivar *ivars = class_copyIvarList([self.object class], &outCount);
-        //Wrap the buffer so that it's automatically freed when we return.
-        NSData *ivarsData = [NSData dataWithBytesNoCopy:ivars length:sizeof(ivars) * outCount freeWhenDone:YES];
-        for (unsigned int i = 0; i < outCount; i++) {
-            Ivar ivar = ivars[i];
-            NSString *ivarName = [NSString stringWithFormat:@"%s", ivar_getName(ivar)];
-            BOOL isMatch = ([ivarName isEqualToString:self.key]) || [ivarName isEqualToString:[@"_" stringByAppendingString:self.key]];
-            if (!isMatch) continue;
-
-            NSString *encoding = [NSString stringWithFormat:@"%s", ivar_getTypeEncoding(ivar)];
-            BOOL isObject = [encoding hasPrefix:@"@"];
-            if (isObject) {
-                NSRange range = NSMakeRange(2, encoding.length-3);
-                NSString *className = [encoding substringWithRange:range];
-                Class class = NSClassFromString(className);
-                NSCAssert([value isKindOfClass:class], ({
-                    [NSString stringWithFormat:@"Attempted to set an object of type <%@> to an ivar of type <%@> for key <%@> of object <%@>.", NSStringFromClass([value class]), NSStringFromClass(class), self.key, self.object];
-                }));
-                return;
-            }
-            //TODO: Check non-object types
-        }
-        //Force the buffer to be retained until the end of the function.
-        [ivarsData self];
-    }();
-#endif
+    assertKVCTargetWillAcceptValue(self.object, self.key, value);
 
     //Validate using KVC
     id validatedValue = value;
     if (![self.object validateValue:&validatedValue forKey:self.key error:outError]) {
-        if (outError != NULL) {
-#pragma message "TODO: in valid value."
-            *outError = [NSError new];
-        }
+        //We don't need to populate outError because validateValue:forKey:error: has already done so.
         return NO;
     }
 
